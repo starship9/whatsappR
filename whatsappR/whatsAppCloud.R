86cff2a571b0102c5a58e66179360ae7a400c3c3
Nishank Saini <nishanksaini10@gmail.com>
2017-02-25 19:58:48 +0530

    Cleaning the data a little bit

--- whatsappR/whatsAppCloud.R
+++ whatsappR/whatsAppCloud.R
@@ -1,9 +1,12 @@
-whatsappRaw <- read.table("INSERT_FILE_HERE.txt", header=FALSE,fill = TRUE)
+whatsappRaw <- read.table("WhatsApp Chat with CSE Junior Year.txt", header=FALSE,fill = TRUE)
 library(dplyr)
 whatsappDF <- tbl_df(whatsappRaw)
 head(whatsappDF) 
 select(whatsappDF,V5)
-
+whatsappDF$V5<-gsub('\\d+', '', whatsappDF$V5)
+whatsappDF$V5<-gsub('\\You+', '', whatsappDF$V5)
 library(wordcloud)
-wordcloud(whatsappDF$V5, random.order = FALSE, max.words = 100)
+wordcloud(whatsappDF$V5, random.order = FALSE, max.words = 100, col = brewer.pal(7,"Accent"))
+
+
 
