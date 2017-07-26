0b20f97a95b0c702c7f40dffe1deb0dd5fcc1d13
starship9 <nishanksaini10@gmail.com>
2017-07-26 13:07:50 +0530

    Added tm functionality

--- whatsappR/whatsAppCloud.R
+++ whatsappR/whatsAppCloud.R
@@ -1,4 +1,32 @@
-whatsappRaw <- read.table("WhatsApp Chat with CSE wishes JunaidDas HBD.txt", header=FALSE,fill = TRUE)
+setwd("C:/Users/Nishank/Desktop/SNU/RStuff/whatsappR/whatsappR")
+
+whatsappRaw <- read.table("WhatsApp Chat with CSE.txt", header=FALSE,fill = TRUE, stringsAsFactors = FALSE)
+View(whatsappRaw)
+str(whatsappRaw)
+library(tm)
+library(RColorBrewer)
+library(tidyverse)
+library(tidytext)
+
+#docs <- VCorpus(DirSource(getwd()))
+#summary(docs)
+
+#inspect(docs[6])
+whatsappRaw <- whatsappRaw[,6:18]
+View(whatsappRaw)
+docs <- Corpus(VectorSource(whatsappRaw))
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs,tolower)
+docs <- tm_map(docs,removeNumbers)
+docs <- tm_map(docs,removeWords,stopwords("english"))
+docs <- tm_map(docs,removeWords,as.vector(as.matrix(stop_words)))
+docs <- tm_map(docs,removeWords,c("happy","birthday","saini","mitra","jose","voruganti","mukhopaddhay","gairola","miglani"))
+
+
+library(wordcloud)
+wordcloud(docs, random.order = FALSE,max.words = 100,random.color = FALSE, colors = brewer.pal(9,"Accent"))
+
+
 library(dplyr)
 whatsappDF <- tbl_df(whatsappRaw)
 head(whatsappDF) 
