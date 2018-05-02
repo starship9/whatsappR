2425222700f9a2b942402dafbe26f1871758148d
starship9 <nishanksaini10@gmail.com>
2018-05-02 16:57:42 +0530

    Redoing in progress

--- whatsappR/whatsAppCloud.R
+++ whatsappR/whatsAppCloud.R
@@ -2,161 +2,57 @@ setwd("C:/Users/Nishank/Desktop/SNU/RStuff/whatsappR/whatsappR")
 
 whatsappRaw <-
   read.table(
-    "WhatsApp Chat with CSE.txt",
+    "WhatsApp Chat with CSE2018.txt",
     header = FALSE,
     fill = TRUE,
     stringsAsFactors = FALSE
   )
-View(whatsappRaw)
-#str(whatsappRaw)
-library(tm)
-library(RColorBrewer)
+tail(whatsappRaw)
+sample(whatsappRaw,3)
+head(whatsappRaw)
+str(whatsappRaw)
+
+class(whatsappRaw)
+#whatsappRaw$V5 <- as.factor(whatsappRaw$V5)
+#table(whatsappRaw$V5)
+
+#basic cleaning
+whatsappRaw$V6[whatsappRaw$V6=="added"] <- ""
+whatsappRaw$V6[is.numeric(whatsappRaw$V6)] <- ""
+whatsappRaw$V6[!is.na(as.numeric(whatsappRaw$V6))] <- ""
+whatsappRaw$V6[whatsappRaw$V6=="removed" | whatsappRaw$V6=="changed"] <- ""
+whatsappRaw$V7[whatsappRaw$V7=="added"] <- ""
+whatsappRaw$V7[whatsappRaw$V7=="removed" | whatsappRaw$V7=="changed"] <- ""
+whatsappRaw$V7[!is.na(as.numeric(whatsappRaw$V7))] <- ""
+
 library(tidyverse)
 library(tidytext)
-#head(whatsappRaw,1)
-#docs <- VCorpus(DirSource(getwd()))
-#summary(docs)
-#inspect(docs[6])
-whatsappRaw <- whatsappRaw[, 7:20]
-#View(whatsappRaw)
-docs <- Corpus(VectorSource(whatsappRaw))
-docs <- tm_map(docs, removePunctuation)
-docs <- tm_map(docs, tolower)
-docs <- tm_map(docs, removeNumbers)
-docs <- tm_map(docs, removeWords, stopwords("english"))
-docs <- tm_map(docs, removeWords, as.vector(as.matrix(stop_words)))
-docs <-
-  tm_map(
-    docs,
-    removeWords,
-    c(
-      "happy",
-      "birthday",
-      "saini",
-      "mitra",
-      "jose",
-      "voruganti",
-      "mukhopaddhay",
-      "gairola",
-      "miglani",
-      "??~"
-    )
-  )
 
+whatsappDF <- whatsappRaw %>% select(V5:V18)
+View(whatsappDF)
 
-library(wordcloud)
+#head(paste(whatsappDF$V7:whatsappDF$V18))
 
-#pdf("shreyCloud.pdf")
-wordcloud(
-  docs,
-  random.order = FALSE,
-  max.words = 100,
-  random.color = FALSE,
-  colors = brewer.pal(9, "Dark2"),
-  scale = c(3, 0.5)
-)
-#dev.off()
+#whatsappChat <-
+# readLines(
+#    "WhatsApp Chat with CSE2018.txt"
+#  )
 
-tdm <- TermDocumentMatrix(docs)
-m <- as.matrix(tdm)
-v <- sort(rowSums(m), decreasing = TRUE)
-d <- data.frame(word = names(v), freq = v)
-head(d, 10)
+#whatsappDFTidy <- data_frame(text = whatsappChat)
+#whatsappDFTidy <- whatsappDFTidy %>% unnest_tokens(word,text) %>% anti_join(stop_words)
 
-wordcloud(
-  words = d$word,
-  freq = d$freq,
-  min.freq = 1,
-  max.words = 100,
-  random.order = FALSE,
-  rot.per = 0.35,
-  colors = brewer.pal(9, "Dark2"),
-  scale = c(3, 0.5)
-)
+#sort(table(whatsappDF$V6)>10)
+whatsappDF$V6[whatsappDF$V6 %in% c("Mitra:","Voruganti:","Pathak:","Miglani:","Saini:","Mukhopaddhay:","Ananth:","Manivanan:","Jose:","Gairola:","C:")] <- ""
 
+#whatsappDF$text <- paste(whatsappDF$(V6:V18))
+#unite(whatsappDF, paste(colnames(whatsappDF)[-1], collapse=" "), colnames(whatsappDF)[-1])
 
-library(dplyr)
-whatsappDF <- tbl_df(whatsappRaw)
-head(whatsappDF)
-select(whatsappDF, V5)
-whatsappDF$V5 <- gsub('\\d+', '', whatsappDF$V5)
-whatsappDF$V5 <- gsub('\\You+', '', whatsappDF$V5)
-library(wordcloud)
-wordcloud(
-  whatsappDF$V5,
-  random.order = FALSE,
-  max.words = 100,
-  col = brewer.pal(7, "Accent")
-)
-whatsappDF$V6 <- gsub('\\Saini+', '', whatsappDF$V6)
-whatsappDF$V6 <- gsub('\\Jose+', '', whatsappDF$V6)
-whatsappDF$V6 <- gsub('\\Mitra+', '', whatsappDF$V6)
-whatsappDF$V6 <- gsub('\\Miglani+', '', whatsappDF$V6)
-whatsappDF$V6 <- gsub('\\d+', '', whatsappDF$V6)
-#newData$combineData<-gsub('\\Saini+', '', newData$combineData)
-#newData$combineData<-gsub('\\Jose+', '', newData$combineData)
-newData <- select(whatsappDF, V6:V20)
+#unite(whatsappDF,text,-1)
+whatsappDFUnite <- unite(whatsappDF,text,-1,sep = " ", remove = TRUE)
+head(whatsappDFUnite)
+View(whatsappDFUnite)
 
-newData <-
-  unite(newData,
-        combineData,
-        V6:V20,
-        sep = " ",
-        remove = TRUE)
-newData$combineData <- gsub('\\d+', '', newData$combineData)
-wordcloud(
-  newData$combineData,
-  random.order = FALSE,
-  max.words = 100,
-  col = brewer.pal(10, "Set3")
-)
+colnames(whatsappDFUnite) <- c("Name","text")
+whatsappTidy <- whatsappDFUnite %>% unnest_tokens(word,text) %>% group_by(Name) %>% anti_join(stop_words) %>% summarise(sum = n)%>% ungroup()
 
-text <-
-  readLines(
-    "C:/Users/Nishank/Desktop/SNU/RStuff/whatsappR/whatsappR/WhatsApp Chat with CSE.txt"
-  )
-head(text)
-tail(text)
-#textDF <- as.data.frame(text)
-library(tidyverse)
-library(tidytext)
-#names(textDF)
-#textDF %>% unnest_tokens(tokens, text)
-textChat <-
-  readLines(
-    "C:/Users/Nishank/Desktop/SNU/RStuff/whatsappR/whatsappR/WhatsApp Chat with CSE.txt"
-  )
-textDF <- data_frame(text = textChat)
-textDF %>% unnest_tokens(tokens, text)
-#textDF$text
-#textDF %>% unnest_tokens(tokens, text)
-#textDF %>% unnest_tokens(text, token)
-#?unnest_tokens
-#textDF <- data_frame(text = textChat)
-textDF
-#textDF %>% unnest_tokens(word, text)
-textDF %>% unnest_tokens(word, text) %>% anti_join(stop_words)
-#tidyWA <- textDF %>% count(word, sort = TRUE)
- tidyWA <-
-   textDF %>% unnest_tokens(word, text) %>% anti_join(stop_words)
-tidyWA %>% count(word, sort = TRUE)
-library(reshape2)
-library(wordcloud)
-library(RColorBrewer)
-tidyWA %>% inner_join(get_sentiments("bing")) %>% count(word, sentiment, sort = TRUE) %>% acast(word ~
-                                                                                                  sentiment, value.var = "n", fill = 0) %>% comparison.cloud(
-                                                                                                    colors = brewer.pal(9,"RdGy"),
-                                                                                                    max.words = 100,
-                                                                                                    scale = c(4, 0.5)
-                                                                                                  )
-
-pdf("comparisonCSE.pdf")
-print(
-  tidyWA %>% inner_join(get_sentiments("bing")) %>% count(word, sentiment, sort = TRUE) %>% acast(word ~
-                                                                                                    sentiment, value.var = "n", fill = 0) %>% comparison.cloud(
-                                                                                                      colors = c("#F8766D", "#00BFC4"),
-                                                                                                      max.words = 100,
-                                                                                                      scale = c(3, 0.5)
-                                                                                                    )
-)
-dev.off()
+whatsappTidy %>% count(word)
